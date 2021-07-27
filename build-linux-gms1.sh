ee76e96d2f55ede0b88bc1e81d0c31e6b131080d
Nikita Krapivin <alienoom@yandex.ru>
2021-07-27 23:07:20 +0500

    Fix some really old compilers (e.g. Ubuntu 14/clang)

--- build-linux-gms1.sh
+++ build-linux-gms1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm libnekopresence-32bit.so
-c++ -m32 -fPIC -g -c -o np.o NekoPresence/nekopresence.cpp
-c++ -m32 np.o -shared -o libnekopresence-32bit.so -L. -ldiscord-rpc-32bit
+c++ -m32 -std=c++11 -fPIC -g -c -o np.o NekoPresence/nekopresence.cpp
+c++ -m32 -std=c++11 np.o -shared -o libnekopresence-32bit.so -L. -ldiscord-rpc-32bit
 rm np.o
